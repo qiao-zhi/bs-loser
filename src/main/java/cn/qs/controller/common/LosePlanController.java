c0e0ab3df5797e6f77b5a9fdd86d38760f5c10c2
qiaozhi <qiao_liqiang@163.com>
2019-04-23 22:50:25 +0800

    init

--- src/main/java/cn/qs/controller/common/LosePlanController.java
+++ src/main/java/cn/qs/controller/common/LosePlanController.java
@@ -1,13 +1,18 @@
 package cn.qs.controller.common;
 
+import org.apache.catalina.servlet4preview.http.HttpServletRequest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.fasterxml.jackson.annotation.JsonCreator.Mode;
 
 import cn.qs.controller.user.UserLosePlanController;
 import cn.qs.service.user.UserService;
+import cn.qs.utils.DefaultValue;
 
 /**
  * 减肥套餐管理
@@ -39,7 +44,9 @@ public class LosePlanController {
 	 * @return
 	 */
 	@RequestMapping("/add")
-	public String add() {
+	public String add(ModelMap map) {
+		map.put("bodys", DefaultValue.BODY_LOCATIONS);
+		map.put("bmis", DefaultValue.BMI_VALUES);
 		return "losePlan/add";
 	}
 }
--- src/main/java/cn/qs/utils/DefaultValue.java
+++ src/main/java/cn/qs/utils/DefaultValue.java
@@ -1,5 +1,7 @@
 package cn.qs.utils;
 
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * 默认值
@@ -23,5 +25,19 @@ public class DefaultValue {
 	 * 移动端token的更新时间(也就是如果更新时间+当前时间大于预计失效时间就更新token)
 	 */
 	public static final int TOKEN_UPDATE_TIME = 1;
+
+	public static final List<String> BODY_LOCATIONS = new ArrayList<String>();
+
+	public static final List<String> BMI_VALUES = new ArrayList<String>();
+
+	static {
+		BODY_LOCATIONS.add("脸部");
+		BODY_LOCATIONS.add("臀部");
+		BODY_LOCATIONS.add("腿部");
+		BODY_LOCATIONS.add("上身");
+
+		BMI_VALUES.add("0-50");
+		BMI_VALUES.add("50-100");
+	}
 
 }
