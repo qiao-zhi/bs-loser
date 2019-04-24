340842831c31b8a47f4d60b55c895ac1780d0e0d
qiao_zhi <qiao_liqiang@163.com>
2019-04-24 19:46:26 +0800

    init

--- src/main/java/cn/qs/controller/common/FileController.java
+++ src/main/java/cn/qs/controller/common/FileController.java
@@ -11,7 +11,6 @@ import org.apache.commons.io.IOUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import cn.qs.utils.FileHandleUtil;
